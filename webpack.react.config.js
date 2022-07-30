1efaa02e3044991785df5eecd05739a3a3dd2400
lhikarul <711hikaru@gmail.com>
2022-07-30 22:14:53 +0800

    refactor: react 腳手架配置

---
+++ webpack.react.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.jsx",
+    // login: "./src/login.js",
+  },
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  devServer: {
+    static: {
+      // directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+        generator: {
+          filename: "images/[name].[hash:6][ext]",
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.resolve(__dirname, "/public/index.html"),
+      chunks: ["index"],
+    }),
+    // new HtmlWebpackPlugin({
+    //   filename: "login.html",
+    //   template: "./src/views/login.html",
+    //   chunks: ["login"],
+    // }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/img"),
+          to: path.resolve(__dirname, "./dist/img"),
+        },
+      ],
+    }),
+  ],
+};
