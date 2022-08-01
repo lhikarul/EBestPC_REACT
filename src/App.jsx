836162256b3a9eff53bec9a600dd3b8b8ab0f364
lhikarul <711hikaru@gmail.com>
2022-08-01 21:13:39 +0800

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import "./css/public.css";
+
 function App() {
-  return <Home />;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "../../index.js";
-function Home() {
+import "../../css/index.css";
+
+function Home(props) {
   return (
     <div>
       <div className="head">
